9627978ab2fe2a8dd11ca10e1a712d65562b3df1
jasons <jasons@novaleaf.com>
2021-01-11 13:38:18 -0800

    end of lesson 11 (complete)

---
+++ DotsMan/Assets/Scripts/Systems/GameStateSystem.cs
@@ -0,0 +1,15 @@
+﻿//using UnityEditor;
+//using UnityEngine;
+using Unity.Entities;
+using UnityEngine;
+
+[AlwaysUpdateSystem]//keep running the OnUpdate() even if there are no entities to work on
+public class GameStateSystem : SystemBase
+{
+	protected override void OnUpdate()
+	{
+		//GetEntityQuery(typeof(Pellet));
+		var pelletQuery = GetEntityQuery(ComponentType.ReadOnly<Pellet>());
+		Debug.Log(pelletQuery.CalculateEntityCount());
+	}
+}
