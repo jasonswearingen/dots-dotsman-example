38824358370318f58e8d79ae526935b6618cf3b5
jasons <jasons@novaleaf.com>
2021-01-16 12:51:53 -0800

    lesson 13 complete: notes on hybrid changes (camera updates)

---
+++ DotsMan/Assets/Scripts/Monobehaviour/CameraAuthoring.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+class CameraAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+	public AudioListener audioListener;
+	public Camera cam;
+
+	public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+	{
+		//dstManager.AddComponent(entity, new CameraTag() { });
+		conversionSystem.AddHybridComponent(audioListener);
+		conversionSystem.AddHybridComponent(cam);
+	}
+}
+
+//public struct CameraTag : ComponentType
+//{
+
+//}
